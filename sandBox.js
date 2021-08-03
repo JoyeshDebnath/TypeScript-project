c1c9e2a869bb70212ec8abec32c3a826731f8b86
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2021-08-03 09:12:46 +0530

    2nd day 2

--- sandBox.js
+++ sandBox.js
@@ -1,14 +1,63 @@
-var character = "SpiderMan";
-console.log(character);
-var age = 20;
-var isCoder = false;
-// character = 200;
-character = "BatMan";
-console.log(character);
+// let character = "SpiderMan";
+// console.log(character);
+// let age = 20;
+// let isCoder = false;
+// // character = 200;
+// character = "BatMan";
+// console.log(character);
+// age = 21;
+// isCoder = true;
+// const Area_circle = (radius:number) => {
+//     return radius * radius * Math.PI;
+// }
+// const area = Area_circle(14.45);
+// console.log(area);
+// let character = ["SpiderMan", "BatMan", "SuperMan", "IronMan"];
+// character.push('Thor');
+// console.log(character);
+// // // character.push(true)
+// // character[1] = "AntMan";
+// // character[1]=12
+// let mixed = ["abc", 123, 'red', 'blue', 'yellow', 456, true];
+// mixed.push("BatMan");
+// mixed.push(100);
+// mixed[0] = false;
+// console.log(mixed);
+// let Player = {
+//     name: "Lionel Messi",
+//     age: 34,
+//     Nation: "Argentina",
+//     IsLegend:true,
+//   skills:["NutMeg","Chop","Penalty","FreeKick","Curl","Dribbling"]
+// }
+// console.log(Player);
+// Player = {
+//     name: "Cristiano Ronaldo",
+//     age:36,
+//     Nation: "Portugal",
+//     IsLegend: true,
+//     skills:["NutMeg","Chop","Penalty","KnuckleShot"]
+// }
+// console.log(Player);
+//Explicit Types................................................................
+var character;
+var age;
+var isProgrammer;
+// age="Twenty One "
 age = 21;
-isCoder = true;
-var Area_circle = function (radius) {
-    return radius * radius * Math.PI;
-};
-var area = Area_circle(14.45);
-console.log(area);
+character = "Joyesh";
+isProgrammer = true;
+//Arrays................................................................
+var names = [];
+names.push("Joyesh");
+console.log(names);
+//Union Types................................................................
+//This lets u to decalre the mixd types ....
+var players = []; //decalring an array to hold string number and boolean type values
+players.push('Zlathan');
+players.push(21);
+players.push(100);
+players.push("Messi");
+players.push(true);
+console.log(players);
+//Objects................................................................
--- sandBox.ts
+++ sandBox.ts
@@ -1,20 +1,82 @@
-let character = "SpiderMan";
-console.log(character);
-let age = 20;
+// let character = "SpiderMan";
+// console.log(character);
+// let age = 20;
 
-let isCoder = false;
+// let isCoder = false;
 
-// character = 200;
+// // character = 200;
 
-character = "BatMan";
-console.log(character);
+// character = "BatMan";
+// console.log(character);
+// age = 21;
+// isCoder = true;
+
+// const Area_circle = (radius:number) => {
+//     return radius * radius * Math.PI;
+// }
+
+// const area = Area_circle(14.45);
+// console.log(area);
+
+
+
+// let character = ["SpiderMan", "BatMan", "SuperMan", "IronMan"];
+
+// character.push('Thor');
+// console.log(character);
+// // // character.push(true)
+// // character[1] = "AntMan";
+// // character[1]=12
+
+// let mixed = ["abc", 123, 'red', 'blue', 'yellow', 456, true];
+// mixed.push("BatMan");
+// mixed.push(100);
+// mixed[0] = false;
+// console.log(mixed);
+
+
+// let Player = {
+//     name: "Lionel Messi",
+//     age: 34,
+//     Nation: "Argentina",
+//     IsLegend:true,
+//   skills:["NutMeg","Chop","Penalty","FreeKick","Curl","Dribbling"]
+// }
+// console.log(Player);
+
+// Player = {
+//     name: "Cristiano Ronaldo",
+//     age:36,
+//     Nation: "Portugal",
+//     IsLegend: true,
+//     skills:["NutMeg","Chop","Penalty","KnuckleShot"]
+// }
+// console.log(Player);
+
+//Explicit Types................................................................
+let character: string;
+let age: number;
+let isProgrammer: boolean;
+// age="Twenty One "
 age = 21;
-isCoder = true;
+character = "Joyesh";
+isProgrammer = true;
 
-const Area_circle = (radius:number) => {
-    return radius * radius * Math.PI;
-}
 
-const area = Area_circle(14.45);
-console.log(area);
 
+//Arrays................................................................
+let names: string[]=[];
+names.push("Joyesh");
+console.log(names);
+
+//Union Types................................................................
+//This lets u to decalre the mixd types ....
+let players: (string | number | boolean)[] = [];//decalring an array to hold string number and boolean type values
+ 
+players.push('Zlathan');
+players.push(21);
+players.push(100);
+players.push("Messi");
+players.push(true);
+ console.log(players);
+//Objects................................................................
